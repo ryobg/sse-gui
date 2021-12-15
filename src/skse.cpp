0d62c3589631413677de5b011d90d627b583b242
ryobg <ryobg@users.noreply.github.com>
2021-12-15 09:01:34 +0200

    Update to SKSE 2.1.4 / SSE 1.6.342 (AE)

--- src/skse.cpp
+++ src/skse.cpp
@@ -194,24 +194,29 @@ handle_skse_message (SKSEMessagingInterface::Message* m)
 
 //--------------------------------------------------------------------------------------------------
 
+consteval std::uint32_t skse_plugin_version () {
+    constexpr std::array<std::uint32_t, 3> ver = {
+#include "../VERSION"
+    };
+    return (ver[0] & 0xFFu << 24) | (ver[1] & 0xFFFu << 12) | (ver[2] & 0xFFFu << 0u);
+};
+
 /// @see SKSE.PluginAPI.h
 
-extern "C" SSEGUI_API bool SSEGUI_CCONV
-SKSEPlugin_Query (SKSEInterface const* skse, PluginInfo* info)
+extern "C" {
+
+SSEGUI_API SKSEPluginVersionData SKSEPlugin_Version =
 {
-    int api;
-    ssegui_version (&api, nullptr, nullptr, nullptr);
+    SKSEPluginVersionData::kVersion,
+    skse_plugin_version (),
+    "SSEGUI",
+    "ryobg",
+    "",
+    SKSEPluginVersionData::kVersionIndependent_Signatures, // Disables the compatibleVersions checks
+    { 0 },
+    0,  // > PACKED_SKSE_VERSION i.e. works with any version of SKSE
+};
 
-    info->infoVersion = PluginInfo::kInfoVersion;
-    info->name = "SSEGUI";
-    info->version = api;
-
-    plugin = skse->GetPluginHandle ();
-
-    if (skse->isEditor)
-        return false;
-
-    return true;
 }
 
 //--------------------------------------------------------------------------------------------------
@@ -254,6 +259,7 @@ SKSEPlugin_Load (SKSEInterface const* skse)
 {
     open_log ();
 
+    plugin = skse->GetPluginHandle ();
     messages = (SKSEMessagingInterface*) skse->QueryInterface (kInterface_Messaging);
     messages->RegisterListener (plugin, "SKSE", handle_skse_message);
 
